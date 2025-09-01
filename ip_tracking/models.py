3533e065defa3c8f826ca897332fd0eb7245d24c
joekariuki3 <joekariuki3@gmail.com>
2025-09-01 11:44:12 +0300

    Add `BlockedIP` model to `ip_tracking` for tracking blocked IP addresses

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -5,3 +5,6 @@ class RequestLog(models.Model):
     ip_address = models.CharField(max_length=20)
     timestamp = models.DateTimeField(auto_now_add=True)
     path = models.CharField(max_length=255)
+
+class BlockedIP(models.Model):
+    ip_address = models.CharField(max_length=20)
