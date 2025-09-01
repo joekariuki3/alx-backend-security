07739cff9704e2502c8ef4e0cf2aa3b45f141088
joekariuki3 <joekariuki3@gmail.com>
2025-09-01 14:21:44 +0300

    Add `country` and `city` fields to `RequestLog` model in `ip_tracking`.

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -5,6 +5,8 @@ class RequestLog(models.Model):
     ip_address = models.CharField(max_length=20)
     timestamp = models.DateTimeField(auto_now_add=True)
     path = models.CharField(max_length=255)
+    country = models.CharField(max_length=255, null=True)
+    city = models.CharField(max_length=255, null=True)
 
 class BlockedIP(models.Model):
     ip_address = models.CharField(max_length=20)
