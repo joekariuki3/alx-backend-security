9d516b30a32769ab6411dfcde45325365cdc8619
joekariuki3 <joekariuki3@gmail.com>
2025-09-01 11:44:47 +0300

    Add management command to block IP addresses in `ip_tracking`

---
+++ ip_tracking/management/commands/block_ip.py
@@ -0,0 +1,23 @@
+from ip_tracking.models import BlockedIP
+from django.core.management.base import BaseCommand
+
+
+"""
+Add ips to BlockedIP
+"""
+class Command(BaseCommand):
+    help = 'Add ips to BlockedIP model'
+
+    def add_arguments(self, parser):
+        parser.add_argument('ip', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        ip_list = options.get('ip', [])
+        if not ip_list:
+            print("No ips provided")
+            return
+        for ip in ip_list:
+            try:
+                BlockedIP.objects.create(ip_address=ip)
+            except Exception as e:
+                print(f"Error creating ip: {e}")
