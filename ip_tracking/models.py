06db79da5a38644f339895fa66af9fcea72ba558
joekariuki3 <joekariuki3@gmail.com>
2025-09-02 10:52:10 +0300

    Switch `ip_address` fields to `GenericIPAddressField`, add `SuspiciousIP` model with `__str__` method

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 
 class RequestLog(models.Model):
-    ip_address = models.CharField(max_length=20)
+    ip_address = models.GenericIPAddressField()
     timestamp = models.DateTimeField(auto_now_add=True)
     path = models.CharField(max_length=255)
     country = models.CharField(max_length=255, null=True)
     city = models.CharField(max_length=255, null=True)
 
 class BlockedIP(models.Model):
-    ip_address = models.CharField(max_length=20)
+    ip_address = models.GenericIPAddressField()
+
+class SuspiciousIP(models.Model):
+    ip_address = models.GenericIPAddressField()
+    reason = models.TextField()
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.reason[:50]}"
